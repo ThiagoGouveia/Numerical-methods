0337be6c9c6fde83158438884d53087a9141ee2b
ThiagoGouveia <thiago.rodrigues.gouveia@gmail.com>
2021-08-03 19:53:07 -0300

    update

--- Linear-Regression.py
+++ Linear-Regression.py
@@ -20,7 +20,7 @@ x_new = np.array([[0],[2],[4],[6]])
 x_new_b = np.c_[np.ones((4, 1)), x_new]
 y_predict = x_new_b.dot(theta_best)
 
-plt.plot(x_new, y_predict, "r-")
+plt.plot(x_new, y_predict, "red")
 plt.plot(x, y, "b.")
 plt.axis([0,2,0,3])
 plt.show()
