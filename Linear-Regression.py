5e9ab0406485cfc4af0b104df18dcf2d0adc8e84
ThiagoGouveia <thiago.rodrigues.gouveia@gmail.com>
2021-09-02 19:32:37 -0300

    numerical integration implementation

--- Linear-Regression.py
+++ Linear-Regression.py
@@ -9,23 +9,23 @@ Created on Tue Aug  3 17:26:24 2021
 import numpy as np
 from sklearn.metrics import r2_score
 import matplotlib.pyplot as plt
+import copy
 
+X = np.array([0.5, 1.2, 2.1, 3.5, 5.4])
+y = np.array([5.1, 3.2, 2.8, 1, 0.4])
 
-X = 2 * np.random.rand(50,1)
-y = X + np.random.rand(50,1)
-
-X = np.c_[np.ones((50, 1)), X]
+X = np.c_[np.ones((5, 1)), X]
 
 theta = np.linalg.inv(X.T.dot(X)).dot(X.T).dot(y)
 
-xNovo = 2 * np.random.rand(50,1)
+xNovo = np.array([0.5, 1.2, 2.1, 3.5, 5.4])
 
-xNovo = np.c_[np.ones((50, 1)), xNovo]
+xNovo = np.c_[np.ones((5, 1)), xNovo]
 yNovo = xNovo.dot(theta)
 
 plt.plot(xNovo[:,1], yNovo, "red")
 plt.plot( X[:,1], y, "b.")
-plt.axis([0,2,0,3])
+#plt.axis([0,2,0,3])
 plt.show()
 
 r2 = r2_score(y, yNovo)
